f6cd6ba925069d22fe793f02aa88df1b9fc8d6dd
Sina-Haz <101819919+Sina-Haz@users.noreply.github.com>
2023-03-19 11:06:18 -0400

    starting the command loop and a process Command function

--- Project 2/mysh.c
+++ Project 2/mysh.c
@@ -7,14 +7,15 @@
 #define MAX_CMD_LENGTH 100
 #define MAX_TOKENS 100
 #define MAX_TOKEN_LENGTH 100
+#define EXTRA 1024
 
 static char buffer[MAX_CMD_LENGTH];
 static char* tokens[MAX_TOKEN_LENGTH];
+static char extraBuffer[EXTRA];
 
 
-
+//Parses command found in Buffer. Sends tokens to tokens: array of strings
 void parseCommand(){
-    //char *delim = " \t\r\n<>|";
     int num_tokens = 0;
     int i = 0;
     int current_token_length = 0;
@@ -52,22 +53,25 @@ void parseCommand(){
     }
 }
 
+//checks for null terminator in buffer
 int getSizeOfCurrCmd(){
     for(int i = 0;i < MAX_CMD_LENGTH;i++){
-        if(buffer[i] == '0'){
+        if(buffer[i] == '\0'){
             return i;
         }
     }
     return MAX_CMD_LENGTH;
 }
 
+//Prints next line in interactive mode. Should recognize if previous command caused an error and add !
 void printNextLine(){
     memset(buffer,0,MAX_CMD_LENGTH);
-    strcpy(buffer,"\nmysh> ");
+    strcpy(buffer,"mysh> ");
     write(STDOUT_FILENO,buffer,getSizeOfCurrCmd());
     memset(buffer,0,MAX_CMD_LENGTH);
 }
 
+//Helper method to print tokens after parsing command
 void printCmds(){
     int i = 0;
     while(i < MAX_TOKEN_LENGTH && tokens[i] != NULL){
@@ -76,18 +80,65 @@ void printCmds(){
     }
 }
 
+//Checks if the input command is longer than the buffer by setting fd to non-blocking mode and
+//trying to read any extra input.
+int CheckForExtraInput(int fd){
+    int flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    ssize_t extra_bytes_read = read(fd,extraBuffer,EXTRA);
+    fcntl(fd,F_SETFL,flags);
+    if(extra_bytes_read != -1){
+        return 1;
+    }
+    memset(extraBuffer,0,EXTRA);
+    return 0;
+}
+
+void processCommand(){
+    char* cmd = tokens[0];
+    if(strcmp(cmd,"cd") == 0){
+        //complete cd process
+    }
+    else if(strcmp(cmd,"pwd") == 0){
+
+    }
+    else if(strcmp(cmd,"ls") == 0){
+
+    }
+    else if(strcmp(cmd,"mkdir") == 0){
+
+    }
+    else if(strcmp(cmd,"cat")){
+
+    }
+    //add more commands that we can process
+}
 
 
-
+void CommandLoop(int fd){
+    ssize_t bytes_read;
+    while(1){
+        bytes_read = read(fd,buffer,MAX_CMD_LENGTH);
+        if(bytes_read == -1 || CheckForExtraInput(fd) == 1){
+            perror("Error with Reading command. Try again\n");
+            printNextLine();
+            continue;
+        }
+        else{
+            if(strcmp(buffer,"exit\n") == 0){break;}
+            parseCommand();
+            printNextLine();
+        }
+    }
+}
 
 void InteractiveShell(){
-    strcpy(buffer,"Welcome to my Terminal");
+    strcpy(buffer,"Welcome to my Terminal\n");
     write(STDOUT_FILENO,buffer,getSizeOfCurrCmd());
     printNextLine();
-    read(STDIN_FILENO,buffer,MAX_CMD_LENGTH);
-    parseCommand();
-    printCmds();
     
+    CommandLoop(STDIN_FILENO);
+
 }
 
 void BatchShell(int fd){
